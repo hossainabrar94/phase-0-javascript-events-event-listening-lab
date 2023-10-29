eac4509f6743c8755f70a19681998343e56c2212
Abrar <hossainabrar@aol.com>
2023-10-29 19:06:13 -0400

    completed

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+const input = document.getElementById('button');
 function addingEventListener() {
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+    input.addEventListener('click', clickAlert);
 }
+input.addEventListener('click', addingEventListener);
